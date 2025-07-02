25215405a11a998e3e3d43585ae792f5653cdb6c
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-07-02 12:14:03 +0530

    Add files via upload

---
+++ task1(tutedude).py
@@ -0,0 +1,10 @@
+First_Number = int(input("Enter the first number: "))
+Second_Number = int(input("Enter the second number: "))
+Addition = First_Number + Second_Number
+Subtraction = First_Number - Second_Number
+Multiplication = First_Number * Second_Number
+Division = First_Number / Second_Number
+print(f"Addition: {Addition}")
+print(f"Subtraction: {Subtraction}")
+print(f"Multiplication: {Multiplication}")
+print(f"Division: {Division}")
---
+++ task2.py
@@ -0,0 +1,3 @@
+First_Name = input("Enter First Name: ").title()
+last_Name = input("Enter last Name: ").title()
+print(f"Hello, {First_Name} {last_Name}! welcome to the python program.")
